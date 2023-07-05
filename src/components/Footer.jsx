9d2f3acd78429fc8b5eb6b1d07472058b90d42ce
yıldız <mesel4461@gmail.com>
2023-07-06 02:37:02 +0300

    as

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, Typography, Container } from "@mui/material";
 import React from "react";
+import handle from "../assets/el.ico";
+import handle2 from "../assets/el2.jpeg";
+import box from "../assets/box2.jpg";
 
 const Footer = () => {
   return (
@@ -17,7 +20,58 @@ const Footer = () => {
         boxShadow:
           "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
       }}
-    ></Box>
+    >
+      <Typography
+        color="#rgb(239, 137, 47)"
+        textAlign="left"
+        sx={{
+          padding: "20px",
+          paddingLeft: "30px",
+          fontSize: "1.2rem",
+          fontWeight: "550",
+        }}
+      >
+        Touch you life goals easy...
+      </Typography>
+      <Container
+        maxWidth="lg"
+        sx={{ display: "flex", justifyContent: "space-evenly" }}
+      >
+        <Box
+          sx={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
+            borderRadius: "10px",
+            width: "25vw",
+            height: "5rem",
+          }}
+        >
+          <img src={handle} width={70} alt="" />
+        </Box>
+        <Box
+          sx={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
+            borderRadius: "10px",
+            width: "25vw",
+            height: "5rem",
+          }}
+        >
+          <img src={handle2} width={70} alt="" />
+        </Box>
+        <Box
+          sx={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
+            borderRadius: "10px",
+            width: "25vw",
+            height: "5rem",
+          }}
+        >
+          <img src={box} width={70} alt="" />
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
