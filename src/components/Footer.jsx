a913abcf6e7f5bfff1578d36bc5aca83a6b28ac3
yıldız <mesel4461@gmail.com>
2023-07-06 03:16:11 +0300

    as

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
-import { Box, Typography, Container } from "@mui/material";
+import { Box, Typography, Container, Button } from "@mui/material";
 import React from "react";
 import handle from "../assets/el.png";
 import handle2 from "../assets/el1.png";
 import box from "../assets/box2.jpg";
+import "./Footer.css";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <Box
       sx={{
@@ -38,44 +41,82 @@ const Footer = () => {
         sx={{ display: "flex", justifyContent: "space-evenly" }}
       >
         <Box
+          component="button"
+          onClick={() => {
+            navigate("/buy");
+          }}
           sx={{
             boxShadow:
               "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
             borderRadius: "10px",
             width: "25vw",
             height: "5rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "30px",
+            paddingLeft: "20px",
+            cursor: "pointer",
           }}
         >
-          <img src={handle} width={70} alt="" />
+          <img src={handle} width={60} alt="" />
           <Typography sx={{ display: "inline-block" }}>
             Buy Advertisement
           </Typography>
         </Box>
         <Box
+          component="button"
+          onClick={() => {
+            navigate("/agents");
+          }}
           sx={{
             boxShadow:
               "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
             borderRadius: "10px",
             width: "25vw",
             height: "5rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "30px",
+            paddingLeft: "20px",
+            cursor: "pointer",
           }}
         >
-          <img src={handle2} width={70} alt="" />
-          <Typography sx={{ display: "inline-block" }}>Be Agent</Typography>
+          <img src={handle2} width={60} alt="" />
+          <Typography
+            sx={{
+              display: "inline-block",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Be Agent
+          </Typography>
         </Box>
         <Box
+          component="button"
+          onClick={() => {
+            navigate("/contactUs");
+          }}
           sx={{
             boxShadow:
               "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
             borderRadius: "10px",
             width: "25vw",
             height: "5rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "30px",
+            paddingLeft: "20px",
+            cursor: "pointer",
           }}
         >
-          <img src={box} width={70} alt="" />
+          <img src={box} width={60} alt="" />
           <Typography sx={{ display: "inline-block" }}>
             Agent Profile
           </Typography>
+          <Button sx={{ padding: "0px" }} variant="contained" color="success">
+            NEW
+          </Button>
         </Box>
       </Container>
     </Box>
